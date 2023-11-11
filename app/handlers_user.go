dc74d7f4e2e52b6cb653e95b13b26aee595f6db3
Blake Duckworth <blake.duckworth@outlook.com>
2023-11-11 12:30:22 -0500

    disable admin user api

--- app/handlers_user.go
+++ app/handlers_user.go
@@ -9,76 +9,76 @@ import (
 )
 
 // Not currently in use
-func (env *Env) HandleUserCreate() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		user := models.User{}
-		if ok := env.decodeRequest(w, r, &user); !ok {
-			return
-		}
-		id, err := env.db.UserCreate(&user)
-		if err != nil {
-			env.encodeResponse(w, &ResponseModel{Message: "Error: " + err.Error()})
-			return
-		}
-		env.encodeResponse(w, &ResponseModel{Message: "success", Data: id})
-	}
-}
+// func (env *Env) HandleUserCreate() http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		user := models.User{}
+// 		if ok := env.decodeRequest(w, r, &user); !ok {
+// 			return
+// 		}
+// 		id, err := env.db.UserCreate(&user)
+// 		if err != nil {
+// 			env.encodeResponse(w, &ResponseModel{Message: "Error: " + err.Error()})
+// 			return
+// 		}
+// 		env.encodeResponse(w, &ResponseModel{Message: "success", Data: id})
+// 	}
+// }
 
 // Not currently in use
-func (env *Env) HandleUserRetrieveAll() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		users, err := env.db.UserRetrieveAll()
-		if err != nil {
-			env.encodeResponse(w, &ResponseModel{Message: "Error: " + err.Error(), Data: nil})
-			return
-		}
-		env.encodeResponse(w, &ResponseModel{Message: "success", Data: users})
-	}
-}
+// func (env *Env) HandleUserRetrieveAll() http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		users, err := env.db.UserRetrieveAll()
+// 		if err != nil {
+// 			env.encodeResponse(w, &ResponseModel{Message: "Error: " + err.Error(), Data: nil})
+// 			return
+// 		}
+// 		env.encodeResponse(w, &ResponseModel{Message: "success", Data: users})
+// 	}
+// }
 
 // Not currently in use
-func (env *Env) HandleUserRetrieveOne() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		params := mux.Vars(r)
-		user_id := params["id"]
-		user, err := env.db.UserRetrieveOne(user_id)
-		if err != nil {
-			env.encodeResponse(w, &ResponseModel{Message: "Error: " + err.Error(), Data: nil})
-			return
-		}
-		env.encodeResponse(w, &ResponseModel{Message: "success", Data: user})
-	}
-}
+// func (env *Env) HandleUserRetrieveOne() http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		params := mux.Vars(r)
+// 		user_id := params["id"]
+// 		user, err := env.db.UserRetrieveOne(user_id)
+// 		if err != nil {
+// 			env.encodeResponse(w, &ResponseModel{Message: "Error: " + err.Error(), Data: nil})
+// 			return
+// 		}
+// 		env.encodeResponse(w, &ResponseModel{Message: "success", Data: user})
+// 	}
+// }
 
 // Not currently in use
-func (env *Env) HandleUserUpdate() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		user := models.User{}
-		if ok := env.decodeRequest(w, r, &user); !ok {
-			return
-		}
-		err := env.db.UserUpdate(&user)
-		if err != nil {
-			env.encodeResponse(w, &ResponseModel{Message: "Error: " + err.Error()})
-			return
-		}
-		env.encodeResponse(w, &ResponseModel{Message: "success", Data: user.ID})
-	}
-}
+// func (env *Env) HandleUserUpdate() http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		user := models.User{}
+// 		if ok := env.decodeRequest(w, r, &user); !ok {
+// 			return
+// 		}
+// 		err := env.db.UserUpdate(&user)
+// 		if err != nil {
+// 			env.encodeResponse(w, &ResponseModel{Message: "Error: " + err.Error()})
+// 			return
+// 		}
+// 		env.encodeResponse(w, &ResponseModel{Message: "success", Data: user.ID})
+// 	}
+// }
 
 // Not currently in use
-func (env *Env) HandleUserDelete() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		params := mux.Vars(r)
-		userId := params["id"]
-		msg, err := env.db.UserDelete(userId)
-		if err != nil {
-			env.encodeResponse(w, &ResponseModel{Message: "Error: " + err.Error(), Data: nil})
-			return
-		}
-		env.encodeResponse(w, &ResponseModel{Message: "success", Data: msg})
-	}
-}
+// func (env *Env) HandleUserDelete() http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		params := mux.Vars(r)
+// 		userId := params["id"]
+// 		msg, err := env.db.UserDelete(userId)
+// 		if err != nil {
+// 			env.encodeResponse(w, &ResponseModel{Message: "Error: " + err.Error(), Data: nil})
+// 			return
+// 		}
+// 		env.encodeResponse(w, &ResponseModel{Message: "success", Data: msg})
+// 	}
+// }
 
 // Returns the already shared users for a wishlist
 func (env *Env) HandleGetSharedUsersForWishlist() http.HandlerFunc {
--- database/user.go
+++ database/user.go
@@ -2,45 +2,44 @@ package database
 
 import (
 	"errors"
-	"strconv"
 
 	"github.com/00Duck/wishr-api/models"
 	"gorm.io/gorm"
 )
 
-func (d *DB) UserCreate(user *models.User) (string, error) {
-	res := d.db.Create(&user)
-	return user.ID, res.Error
-}
+// func (d *DB) UserCreate(user *models.User) (string, error) {
+// 	res := d.db.Create(&user)
+// 	return user.ID, res.Error
+// }
 
-func (d *DB) UserUpdate(user *models.User) error {
-	d.db.First(&models.User{}, "id = ?", user.ID)
-	res := d.db.Save(&user)
-	return res.Error
-}
+// func (d *DB) UserUpdate(user *models.User) error {
+// 	d.db.First(&models.User{}, "id = ?", user.ID)
+// 	res := d.db.Save(&user)
+// 	return res.Error
+// }
 
-func (d *DB) UserRetrieveOne(id string) (*models.User, error) {
-	user := &models.User{}
-	res := d.db.First(user, "id = ?", id)
-	if res.RowsAffected == 0 {
-		return nil, errors.New("No record found")
-	}
-	return user, res.Error
-}
+// func (d *DB) UserRetrieveOne(id string) (*models.User, error) {
+// 	user := &models.User{}
+// 	res := d.db.First(user, "id = ?", id)
+// 	if res.RowsAffected == 0 {
+// 		return nil, errors.New("No record found")
+// 	}
+// 	return user, res.Error
+// }
 
-func (d *DB) UserRetrieveAll() ([]models.User, error) {
-	users := []models.User{}
-	res := d.db.Find(&users)
-	return users, res.Error
-}
+// func (d *DB) UserRetrieveAll() ([]models.User, error) {
+// 	users := []models.User{}
+// 	res := d.db.Find(&users)
+// 	return users, res.Error
+// }
 
-func (d *DB) UserDelete(id string) (string, error) {
-	res := d.db.Delete(&models.User{ID: id})
-	if res.RowsAffected == 0 {
-		return "", errors.New("No record found to delete")
-	}
-	return strconv.FormatInt(res.RowsAffected, 10) + " rows deleted", res.Error
-}
+// func (d *DB) UserDelete(id string) (string, error) {
+// 	res := d.db.Delete(&models.User{ID: id})
+// 	if res.RowsAffected == 0 {
+// 		return "", errors.New("No record found to delete")
+// 	}
+// 	return strconv.FormatInt(res.RowsAffected, 10) + " rows deleted", res.Error
+// }
 
 func (d *DB) GetUsersForWishlist(wishlistID string) ([]models.SearchUser, error) {
 	users := []models.SearchUser{}
